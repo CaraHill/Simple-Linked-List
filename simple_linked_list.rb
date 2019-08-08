bdbc022d039ba5b4a9bea928e03d4b849e3febc8
Cara Hill <cara@rabidtech.co.nz>
2019-08-08 14:05:59 +1200

    Final refactor with working, dynamic solution.

--- simple_linked_list.rb
+++ simple_linked_list.rb
@@ -13,26 +13,28 @@ end
 
 # Simple Linked List class
 class SimpleLinkedList
-  def initialize(passed_in_array = nil)
+  def initialize(passed_in_array = [])
     @head = nil
     @array = []
-    assign_passed_in_array_values(passed_in_array) if passed_in_array
+    assign_passed_in_array_values(passed_in_array)
   end
 
   def push(element)
-    return assign_head(element) if @head.nil?
+    element.next = @head unless @head.nil?
+    @head = element
 
-    assign_next(element) unless @head.nil?
+    @array << @head.datum
+
+    self
   end
 
   def pop
     return @head if @head.nil?
 
-    return pop_head if @head.next.nil?
-
-    return pop_head_next if @head.next.next.nil?
-
-    pop_head_next_next
+    element = @head
+    @head = element.next
+    element.next = nil
+    element
   end
 
   def to_a
@@ -40,66 +42,19 @@ class SimpleLinkedList
   end
 
   def reverse!
-    return reverse_head unless @array.empty?
-
-    return self if @array.empty?
+    @array = []
+    temp_array = []
+    temp_array << pop until @head.nil?
+    assign_passed_in_array_values(temp_array)
+    self
   end
 
   private
 
-  def pop_head
-    pop_value = @head
-    @head = nil
-    pop_value
-  end
-
-  def pop_head_next
-    pop_value = @head.next
-    @head.next = nil
-    pop_value
-  end
-
-  def pop_head_next_next
-    pop_value = @head.next.next
-    @head.next.next = nil
-    pop_value
-  end
-
-  def assign_head(element)
-    @head = element
-    @array << @head.datum
-    self
-  end
-
-  def assign_next(element)
-    if @head.next.nil?
-      @head.next = element
-      @array << @head.next.datum
-      return self
-    end
-
-    unless @head.next.nil?
-      @head.next.next = element
-      @array << @head.next.next.datum
-    end
-
-    self
-  end
-
-  def reverse_head
-    unless @head.next.next
-      @head.next.next = @head
-      @head = @head.next
-      @head.next.next = nil
-    end
-
-    @array
-  end
-
   def assign_passed_in_array_values(passed_in_array)
-    passed_in_array.each do |number|
-      element = Element.new(number)
-      push(element)
+    passed_in_array.each do |element|
+      push(Element.new(element)) if element.is_a?(Integer)
+      push(element) if element.is_a?(Element)
     end
   end
 end
